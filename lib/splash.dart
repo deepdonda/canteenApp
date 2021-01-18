2f94186b61ce26df10a115dd9fcaf962cf8334d6
shekhutsav1962001 <shekhutsav1962001@gmail.com>
2021-01-18 14:43:41 +0530

    splash screen and navbar created

---
+++ lib/splash.dart
@@ -0,0 +1,75 @@
+import 'dart:async';
+
+import 'package:canteen/main.dart';
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatefulWidget {
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    
+    super.initState();
+    Timer(Duration(seconds: 4), () =>
+    //  MyNavigator.goToHome(context)    
+    Navigator.of(context).pushReplacement(MaterialPageRoute(
+            builder: (BuildContext context) => HomePage())));
+     
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    print("again");
+    return Scaffold(
+      body: Stack(
+        fit: StackFit.expand,
+        children: <Widget>[
+          Container(
+            decoration: BoxDecoration(color: Colors.white)
+          ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: <Widget>[
+              Expanded(
+                flex: 2,
+                child: Container(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Padding(
+                        padding: EdgeInsets.only(top: 200.0),
+                      ),
+                      Image.asset("assets/img/logo.png",height: 200,),
+                      Text(
+                        "FoodZone",
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 24.0),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+              Expanded(
+                flex: 1,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    CircularProgressIndicator( valueColor: new AlwaysStoppedAnimation<Color>(Colors.orange),),
+                    Padding(
+                      padding: EdgeInsets.only(top: 20.0),
+                    ),
+                  ],
+                ),
+              )
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
