928e23cf4fd03a4493a3cb979218a07592a49bee
Deep Donda <deepdonda007@gmail.com>
2021-02-23 21:56:16 +0530

    payment success/failer pages added

--- lib/main.dart
+++ lib/main.dart
@@ -45,6 +45,7 @@ class _HomePageState extends State<HomePage> {
     
     super.initState();
    // establishConnection();
+    //print(DateTime.now().toString().replaceAll(' ', '_'));
     gettoken();
     
     
